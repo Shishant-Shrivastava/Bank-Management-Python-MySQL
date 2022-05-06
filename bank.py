bb2d7634679306c793e8c240dbe1158ae9591506
Shishant-Shrivastava <85758031+Shishant-Shrivastava@users.noreply.github.com>
2022-05-06 15:36:36 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,8 +1,8 @@
 import mysql.connector
 mydb = mysql.connector.connect(
     host = 'localhost',
-    user = 'root',
-    password = '@Naruto123',
+    user = ' ',
+    password = ' ',
     database = 'bankmanagement'
 )
 
